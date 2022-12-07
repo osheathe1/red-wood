fd072aafd4691ec60e265764af03b22bd18dc436
Spoogh <joshuafloresjjf@gmail.com>
2022-12-07 14:39:08 -0600

    added cart component and queries to go along with it

---
+++ client/src/pages/Cart.js
@@ -0,0 +1,15 @@
+
+
+// const [cart, setCart]= useState()
+
+// const addToCart = (productId, variantInfo) =>{
+
+//     if (variantInfo){
+//         commerce.cart.add(productId, 1, variantInfo)
+//         .then(res=>{
+//             setCart(res.cart)
+//         })
+//     } else{
+//         window.alert('idk what to put yet')
+//     }
+// }
