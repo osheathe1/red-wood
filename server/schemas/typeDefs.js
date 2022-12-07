50cecf1b8945e94c8aa1f25b2acd836b3e5755ec
Matt <matthewthomsen99@gmail.com>
2022-12-07 13:16:01 -0600

    add queries and mutations to typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -44,18 +44,22 @@ const typeDefs = gql`
   type Query {
     users: [User]
     user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
+    products: [Product]
+    product(productId: ID!): Product
+    vendors: [Vendor]
+    vendor(vendorId: ID!): Vendor
     me: User
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addReview(text: String!, stars: Int!, productId: ID!): Product
+    removeReview(reviewId: ID!): Product
+    addProduct(name: String!, description: String!, price: Float!, category: String!, quantityInStock: Int!, image: String!, vendor: String!): Product
+    addVendor(name: String!): Vendor
+    addFavorite(productId: ID!): User
+    removeFavorite(productId: ID!): User
   }
 `;
 
