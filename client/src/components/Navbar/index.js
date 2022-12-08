1cc4655701ae9ca0f03539248da854048df6ecac
Zachary Schroeder <zschroeder34@gmail.com>
2022-12-08 11:29:37 -0600

    Fixed error

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Navbar.css";
 import SearchBar from "../SearchBar/searchBar";
-import ProductData from '../../../../server/seeders/productSeeds.json'
+// import ProductData from '../../../../server/seeders/productSeeds.json'
 function Navbar() {
   return (
     <div className="nav">
       <div className="links">
         <Link to="/"> Home </Link>
         <Link to="/categories"> Categories </Link>
-        <SearchBar placeholder='Search' data={ProductData}/>
+        <SearchBar placeholder='Search' />
         <Link to="/login"> Login </Link>
         <Link to="/cart"> Cart </Link>
       </div>
