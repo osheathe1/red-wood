7777abf6080769a3893ead8650115bd54cc3b85e
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-09 12:59:00 -0600

    Fixed bug in App

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Profile from './pages/Profile';
 import Footer from './components/Footer';
 import { Product } from './components/Product';
 import AboutUs from './pages/AboutUs';
-import Cart from './components/Cart';
+import Cart from './components/CartItem';
 import { StoreProvider } from './utils/GlobalState';
 
 // Construct our main GraphQL API endpoint
