908405880f64e3cf694cf85a96d146de172cf92f
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-09 19:25:12 -0600

    Fixed me query and changed navbar to link to profile when logged in

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,14 +19,9 @@ export const QUERY_ME = gql`
   query me {
     me {
       _id
-      username
       email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
-        createdAt
-      }
+      password
+      username
     }
   }
 `;
