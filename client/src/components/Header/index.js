fa1339944f896bf91d67055f9f77b1162b71a01b
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-10 17:02:39 -0600

    Not sure what this is

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,6 +10,21 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
+
+  // const main_nav = document.querySelector('.guiding-arrow');
+
+  // window.addEventListener("scroll", () => {
+  //     var y = window.scrollY;
+  //     if (y >= 1){
+  //         main_nav.classList.add('disappear');
+  //         return;
+  //     }
+  //     else{
+  //         main_nav.classList.remove('disappear');
+  //         // note that this is a class defined in your CSS.
+  //     }
+  // });
+
   return (
     <header className="custom-bg-color text-light mb-4 py-3 flex-column justify-center align-center custom-header">
       <div className="container flex-row justify-center align-center">
@@ -17,29 +32,7 @@ const Header = () => {
           <Link className="text-light" to="/">
             <img src={logo} alt='redwood-logo' className="m-0 custom-title"></img>
           </Link>
-          {/* <p className="m-0 custom-sub-title">Biggest wood in town!</p> */}
         </div>
-        {/* <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {Auth.getProfile().data.username}'s profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div> */}
       </div>
       <div className='guiding-arrow'><span>&#8595;</span></div>
     </header>
