5d37be739b79e21b935a1c5c7b89e8008ab0fdcf
Matt <matthewthomsen99@gmail.com>
2022-12-13 15:22:00 -0600

    tryin lil sum

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/redwood-db',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
   }
 );
 
