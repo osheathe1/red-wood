2b0c0b734330c46729d589ba7508647acc03103b
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-12-13 10:50:55 -0600

    Added search url for search results

--- client/src/components/HeroCard/index.js
+++ client/src/components/HeroCard/index.js
@@ -4,13 +4,16 @@ export const HeroCard = (props) => {
 
   // pull random product from props.products
   const randomProduct = props.products[Math.floor(Math.random() * props.products.length)];
-  // console.log(randomProduct)
+  console.log(randomProduct)
 
   // const productId = '60f1b0f1b3b1a8b0b0c1c1c1'
   let productUrl = `/product/${randomProduct._id}`
+  let backgroundImgUrl = `url(${randomProduct.image})`
 
   return (
-    <div className='hero-card-container'>
+    <div className='hero-card-container' style={{
+      backgroundImage: backgroundImgUrl,
+    }}>
       <p className='hero-card-title'>{randomProduct.name}</p>
       <a className='hero-card-link' href={productUrl} alt='hero-link'>Buy Now!</a>
     </div>
